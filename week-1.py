498e93b729a3bc3c353a14570e07ff23f89c49f7
Rujipas-Varathikul <deadbook60@gmail.com>
2025-07-17 16:25:04 +0700

    first commit

---
+++ week-1.py
@@ -0,0 +1,11 @@
+##################################################
+# What are we talking about today?               #
+# 1. Get to know each other                      #
+# 2. What is coding/programming. What is Python? #
+# 3. Writing your first code.                    #
+# 4. comment                                     #
+# 5. print function                              #
+# 6. Codeforces                                  #
+# 7. Basic Data Types                            #
+# 8. Variables                                   #
+##################################################
