7bc2bcb46319b342361878d8394c036631fac190
Rujipas-Varathikul <deadbook60@gmail.com>
2025-07-19 12:08:32 +0700

    First week lecture

--- week-1.py
+++ week-1.py
@@ -9,3 +9,42 @@
 # 7. Basic Data Types                            #
 # 8. Variables                                   #
 ##################################################
+
+# This function is used for getting customer data
+# This line will be commented out
+"""
+This is also a comment.
+But it is multiline
+"""
+# This should be commented out
+
+# function => name(input_1, input_2, input_3)
+# argument => input of the function
+print(12)
+print('Hello') # This function print value to the terminal
+print(12, 5, 6)
+print('Hello', 5)
+
+# Codeforces: duep1g22025.contest.codeforces.com
+
+print('Hello, World!')
+
+# Primitive Data Types
+
+# Integer (int)
+print(12, 5, 0, -7)
+
+print(type(5))
+
+# Decimal/Floating point number (float)
+print(1.25, -2.9, 5.00)
+print(5.00, type(5.00))
+
+# String (str)
+print('Hello', "Due", '300 25', 'print()')
+
+# Boolean (bool)
+print(True, False)
+
+# NoneType
+print(None)
