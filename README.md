d2a3336d61108a3a41716526abdb78142c9f9ad7
Rujipas-Varathikul <deadbook60@gmail.com>
2025-07-25 15:10:47 +0700

    Update for week 2 lesson

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-**Leagues of Code TH: Python Level 1 July 2025 Course**
+# Leagues of Code TH: Python Level 1 July 2025 Course
+This repository is used for collecting teaching materials for Python Level 1 Course. You can use the list of topic in the following section to recap your knowledge.
+
+## Topics Covered
+1. comment
+2. print function
+3. Basic Data Types
---
+++ week-2.py
@@ -0,0 +1,12 @@
+##################################################
+# What are we talking about today?               #
+# 1. Variables                                   #
+# 2. How to receive input?                       #
+# 3. Arithmetic Operators                        #
+# 4. Variable Adjustment                         #
+# 5. Comparison Operators                        #
+# 6. Boolean Operators                           #
+# 7. Basic Logic                                 #
+# 8. Built-In Python Functions for Numbers       #
+##################################################
+
